462cbe4e0e4e7c9964afa856ddbf85253cd2287b
Bhaskor Jyoti Sarmah <bhaskor88@gmail.com>
2019-06-29 11:46:10 +0530

    Validation completed with proper messaging for Project Model

--- ppmtool/src/main/java/io/bhaskor/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/io/bhaskor/ppmtool/web/ProjectController.java
@@ -1,14 +1,18 @@
 package io.bhaskor.ppmtool.web;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.bhaskor.ppmtool.domain.Project;
+import io.bhaskor.ppmtool.services.MapValidationErrorService;
 import io.bhaskor.ppmtool.services.ProjectService;
 
 @RestController
@@ -17,9 +21,19 @@ public class ProjectController{
 
     @Autowired
     private ProjectService projectService;
+    
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+    	
+    	ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationService(result);
+    	
+    	if(null != errorMap) {
+    		return errorMap;
+    	}
+    	
     	Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
