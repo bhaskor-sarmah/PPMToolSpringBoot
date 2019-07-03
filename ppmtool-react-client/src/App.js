dcc152de6d9a361bd100c916a219396e90c48242
Bhaskor Jyoti Sarmah <bhaskor88@gmail.com>
2019-07-03 15:07:25 +0530

    React App - React Router Dom included and First route created

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -3,14 +3,19 @@ import './App.css'
 import Dashboard from './components/Dashboard'
 import Header from './components/layout/Header'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import AddProject from './components/project/AddProject';
 
 class App extends Component {
-  render () {
+  render() {
     return (
-      <div className='App'>
-        <Header />
-        <Dashboard />
-      </div>
+      <Router>
+        <div className='App'>
+          <Header />
+          <Route exact path='/dashboard' component={Dashboard} />
+          <Route exact path='/addProject' component={AddProject} />
+        </div>
+      </Router>
     )
   }
 }
