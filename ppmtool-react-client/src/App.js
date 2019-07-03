6c273ee0e0378afa5c21c5530a3b6d4440899ec2
Bhaskor Jyoti Sarmah <bhaskor88@gmail.com>
2019-07-03 14:14:20 +0530

    React App - Dashboard design with bootstrap and Font awesome

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import './App.css'
 import Dashboard from './components/Dashboard'
+import Header from './components/layout/Header'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div className='App'>
+        <Header />
         <Dashboard />
       </div>
     )
--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react'
+import ProjectItem from './project/ProjectItem'
 
 class Dashboard extends Component {
-  render() {
-    return <h1>Welcome to the dashboard</h1>
+  render () {
+    return (
+      <div className='projects'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-12'>
+              <h1 className='display-4 text-center'>Projects</h1>
+              <br />
+              <a href='ProjectForm.html' className='btn btn-lg btn-info'>
+                Create a Project
+              </a>
+              <br />
+              <hr />
+              <ProjectItem />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
